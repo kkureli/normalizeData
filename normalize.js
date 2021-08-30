6616330a3145618ccd3789afcab6436eb672673e
Kaan Kureli <33238066+kkureli@users.noreply.github.com>
2021-08-30 19:10:19 +0300

    fix normalize.js for array type

--- normalize.js
+++ normalize.js
@@ -1,7 +1,21 @@
+const obj = {};
+
 const normalize = (data) => {
-  const obj = {}
-    for (const key in data) {
-      obj[key] = data[key].value
+  for (const key in data) {
+    if (Array.isArray(data[key].value)) {
+      let valueArray = [];
+
+      data[key].value.map((e) => {
+        const valuesObj = {};
+        for (const v in e) {
+          valuesObj[v] = e[v].value;
+        }
+        valueArray.push(valuesObj);
+        obj[key] = valueArray;
+      });
+    } else {
+      obj[key] = data[key].value;
     }
-  return obj
-}
+  }
+  return obj;
+};
